0a510652800066ba8e2dbdefe8079e8d4cade401
rbezerrajc <rbezerra@sjcc.com.br>
2022-09-13 10:39:27 -0300

    temporizador

--- desafios-part01/js/script.js
+++ desafios-part01/js/script.js
@@ -3,19 +3,21 @@ function insert(num) {
   document.getElementById('resultado').innerHTML = numero + num;
 }
 function clean() {
-  document.getElementById('resultado').innerHTML = "";
+  document.getElementById('resultado').innerHTML = '';
 }
 function back() {
   var resultado = document.getElementById('resultado').innerHTML;
-  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+  document.getElementById('resultado').innerHTML = resultado.substring(
+    0,
+    resultado.length - 1,
+  );
 }
 function calcular() {
   var resultado = document.getElementById('resultado').innerHTML;
   if (resultado) {
     document.getElementById('resultado').innerHTML = eval(resultado);
-  }
-  else {
-    document.getElementById('resultado').innerHTML = "Nada..."
+  } else {
+    document.getElementById('resultado').innerHTML = 'Nada...';
   }
 }
 
@@ -48,7 +50,29 @@ var mes = [
 
 document.getElementById('dia-semana').innerHTML = semana[d.getDay()];
 const ano = (document.getElementById('ano').innerHTML = d.getFullYear());
-const mesAtual = (document.getElementById('mes').innerHTML =
-  mes[d.getMonth()]);
-const diaAtual = (document.getElementById('dia-mes').innerHTML =
-  d.getDate());
+const mesAtual = (document.getElementById('mes').innerHTML = mes[d.getMonth()]);
+const diaAtual = (document.getElementById('dia-mes').innerHTML = d.getDate());
+
+// contador
+
+// function startTimer(duration, display) {
+//   var timer = duration,
+//     minutes,
+//     seconds;
+//   setInterval(function () {
+//     minutes = parseInt(timer / 60, 10);
+//     seconds = parseInt(timer % 60, 10);
+//     minutes = minutes < 10 ? '0' + minutes : minutes;
+//     seconds = seconds < 10 ? '0' + seconds : seconds;
+//     display.innerHTML = minutes + ':' + seconds;
+//     if (--timer < 0) {
+//       timer = duration;
+//     }
+//   }, 1000);
+// }
+// window.onload = function () {
+//   var duration = 60 * 5; // Converter para segundos
+//   display = document.querySelector('#timer').innerHTML =
+//     minutos + ':' + segundos; // selecionando o timer
+//   startTimer(duration, display); // iniciando o timer
+// };
