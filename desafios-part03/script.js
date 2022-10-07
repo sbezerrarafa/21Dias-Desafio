63b55fa42d50c37ac7f5de9290b9577b18ad3b78
rbezerrajc <rbezerra@sjcc.com.br>
2022-10-07 10:48:09 -0300

    valor digitado

--- desafios-part03/script.js
+++ desafios-part03/script.js
@@ -6,9 +6,16 @@ var cont = 0;
 var disp = document.getElementById("display");
 var btn = document.getElementById('btn-carrinho')
 
+
 function myFunction() {
   console.log('entrou');
   cont++;
   $('.fn-carrinho').text(cont);
 };
 
+
+document.addEventListener("keydown", function (event) {
+  const tecla = event.key
+
+  $("#digitado").text(tecla)
+})
